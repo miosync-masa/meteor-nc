5b6ba53f292b0b81c1f4405a6a7a83fc4e39c839
MASAMICHI IIZUMI <m.iizumi@miosync.email>
2026-01-15 02:06:49 +0900

    Organize imports and define module exports
    Refactor imports and update __all__ for better module structure.

--- meteor_nc/cryptography/__init__.py
+++ meteor_nc/cryptography/__init__.py
@@ -3,10 +3,40 @@
 Meteor-NC Cryptography Module
 """
 
-from .common import *
-from .core import *
+from .common import (
+    HKDF,
+    Q_DEFAULT,
+    MSG_BYTES,
+    MSG_BITS,
+    SECURITY_PARAMS,
+    GPU_AVAILABLE,
+    CRYPTO_AVAILABLE,
+)
 
+from .core import (
+    LWEKEM,
+    HybridKEM,
+    SymmetricMixer,
+)
+
+# Batch KEM (GPU required)
 try:
     from .batch import BatchLWEKEM
+    BATCH_AVAILABLE = True
 except ImportError:
-    pass
+    BATCH_AVAILABLE = False
+
+__all__ = [
+    "HKDF",
+    "LWEKEM",
+    "HybridKEM",
+    "SymmetricMixer",
+    "BatchLWEKEM",
+    "Q_DEFAULT",
+    "MSG_BYTES",
+    "MSG_BITS",
+    "SECURITY_PARAMS",
+    "GPU_AVAILABLE",
+    "CRYPTO_AVAILABLE",
+    "BATCH_AVAILABLE",
+]
