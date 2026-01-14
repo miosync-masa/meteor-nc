8ff2d60e918cae9417efe869541ecdfce33fc635
MASAMICHI IIZUMI <m.iizumi@miosync.email>
2026-01-15 02:05:50 +0900

    Update __init__.py

--- meteor_nc/cryptography/kernels/__init__.py
+++ meteor_nc/cryptography/kernels/__init__.py
@@ -1,4 +1,23 @@
 # meteor_nc/cryptography/kernels/__init__.py
+"""
+Meteor-NC CUDA Kernels
+"""
+
 from .blake3_kernel import GPUBlake3
 
-__all__ = ["GPUBlake3"]
+from .batch_kernels import (
+    cbd_i32,
+    matmul_AT_R,
+    bdot_R,
+    b_from_As,
+    sdot_U,
+)
+
+__all__ = [
+    "GPUBlake3",
+    "cbd_i32",
+    "matmul_AT_R",
+    "bdot_R",
+    "b_from_As",
+    "sdot_U",
+]
